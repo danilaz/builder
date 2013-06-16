2ca4002717d4018b2f120a4556c5b78e56c5de73
danilaz <danilaz.spb@mail.ru>
2013-06-16 23:39:47 +0400

    Config back

--- config/config.inc.php
+++ config/config.inc.php
@@ -1,7 +1,7 @@
-﻿<?php
-		$config['dbhost'] = 'mysql-b2bbuilder.jelastic.regruhosting.ru';      //数据库所在IP地址
-		$config['dbuser'] = 'b2bbuilder';  //数据库用户
+<?php
+		$config['dbhost'] = 'localhost';      //数据库所在IP地址
+		$config['dbuser'] = 'ekolobok';  //数据库用户
 		$config['dbpass'] = 'admin';   	 //数据库密码
-		$config['dbname'] = 'b2bbuilder';     //数据库名
+		$config['dbname'] = 'ekolobok';     //数据库名
 		$config['table_pre']='b2bbuilder_';  //数据库表前缀
 		$config['authkey']='226976b471877b3fbe0b12ad82af8411';  //数据库表前缀?>
