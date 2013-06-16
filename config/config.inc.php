888b8edcb5ff1893a91f0c3466f123aed9374dbc
danilaz <danilaz.spb@mail.ru>
2013-06-17 00:00:55 +0400

    local config

--- config/config.inc.php
+++ config/config.inc.php
@@ -1,5 +1,5 @@
-﻿<?php
-		$config['dbhost'] = 'mysql-b2bbuilder.jelastic.regruhosting.ru';      //数据库所在IP地址
+<?php
+		$config['dbhost'] = 'localhost';      //数据库所在IP地址
 		$config['dbuser'] = 'ekolobok';  //数据库用户
 		$config['dbpass'] = 'admin';   	 //数据库密码
 		$config['dbname'] = 'ekolobok';     //数据库名
