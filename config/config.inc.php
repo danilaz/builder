3afbf375daf498aa8e99041dd9c25ddbf810ef5f
danilaz <danilaz.spb@mail.ru>
2017-04-06 00:01:43 +0300

    Update config.inc.php

--- config/config.inc.php
+++ config/config.inc.php
@@ -1,5 +1,5 @@
 <?php
-		$config['dbhost'] = 'localhost';      //数据库所在IP地址
+		$config['dbhost'] = 'node28661-builder.jelastic.regruhosting.ru';      //数据库所在IP地址
 		$config['dbuser'] = 'ekolobok';  //数据库用户
 		$config['dbpass'] = 'admin';   	 //数据库密码
 		$config['dbname'] = 'ekolobok';     //数据库名
